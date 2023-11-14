2ab512f2faa80469814fb502aec0783ac10ec4e4
Aviv Hazan <aviv6715@gmail.com>
2023-11-14 23:32:31 +0200

    create server and tests

--- file-saving-microservice/file_saving_microservice_server.py
+++ file-saving-microservice/file_saving_microservice_server.py
@@ -6,7 +6,7 @@ import file_saving_microservice_pb2
 import file_saving_microservice_pb2_grpc 
 
 
-REGION_ID = ""
+REGION_ID = "x"
 CREDENTIALS_FILE = ""
 
 
@@ -21,6 +21,77 @@ class FileCloudAccessServicer(file_saving_microservice_pb2_grpc.FileCloudAccessS
         """
         self._file_storage_manager = file_storage_manager
 
+
+    def UploadFile(self, request_iterator, context):
+        """Upload a file to the cloud.
+
+        Args:
+            request_iterator: An iterator of UploadFileRequest.
+            context: The RPC context.
+
+        Returns:
+            UploadFileResponse: The response indicating the success of the operation.
+        """
+        for request in request_iterator:
+            file_name = request.file_name
+            file_data = request.file_data
+            file_type = request.type
+
+            # Call the file storage manager to upload the file
+            self._file_storage_manager.upload_file(file_data, file_name, file_type)
+
+        response = file_saving_microservice_pb2.UploadFileResponse()
+        return response
+
+
+    def DownloadFile(self, request, context):
+        """Download a file from the cloud.
+
+        Args:
+            request: DownloadFileRequest containing the file name.
+            context: The RPC context.
+
+        Yields:
+            DownloadFileResponse: The response containing the file data.
+        """
+        try:
+            # Download the file from the file storage manager
+            file_data = self._file_storage_manager.download_file(request.file_name)
+
+            if file_data is not None:
+                response = file_saving_microservice_pb2.DownloadFileResponse(file_data=file_data)
+                yield response
+            else:
+                context.set_code(grpc.StatusCode.NOT_FOUND)
+                context.set_details("Error: File not found")
+        except Exception as e:
+            # Handle other exceptions (e.g., network errors, unexpected issues)
+            context.set_code(grpc.StatusCode.UNKNOWN)
+            context.set_details(f"Error: {str(e)}")
+            
+
+    def DeleteFile(self, request, context):
+        """Delete a file from the cloud.
+
+        Args:
+            request: DeleteFileRequest containing the file name.
+            context: The RPC context.
+
+        Returns:
+            DeleteFileResponse: The response indicating the success of the operation.
+        """
+        response = file_saving_microservice_pb2.DeleteFileResponse()
+        try:
+            # Delete the file using the file storage manager
+            self._file_storage_manager.delete_file(request.file_name)
+        except FileNotFoundError:
+            context.set_code(grpc.StatusCode.NOT_FOUND)
+            context.set_details("Error: File not found")
+        except Exception as e:
+            context.set_code(grpc.StatusCode.UNKNOWN)
+            context.set_details(f"Error: {str(e)}")
+
+        return response
 
 
 def serve():
