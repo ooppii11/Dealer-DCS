b7bd0dc31cf7233f657aac32b0ba0471cf362c3a
Aviv Hazan <aviv6715@gmail.com>
2023-11-30 18:05:17 +0200

    add cloud grpc proto

---
+++ cloud-server/Services/CloudGrpsService.cs
@@ -0,0 +1,18 @@
+﻿using Grpc.Core;
+using GrpcCloud;
+using System.Threading;
+
+namespace cloud_server.Services
+{
+    public class CloudGrpsService: GrpcCloud.Cloud.CloudBase
+    {
+        private readonly ILogger<CloudGrpsService> _logger;
+
+        public CloudGrpsService(ILogger<CloudGrpsService> logger)
+        {
+            _logger = logger;
+        }
+
+       
+    }
+}
