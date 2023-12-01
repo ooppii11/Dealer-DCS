8390528e39618e9648c7d70cca1d670cf1c3ea5d
Aviv Hazan <aviv6715@gmail.com>
2023-12-01 17:22:53 +0200

    Add signup method to the server

--- cloud-server/Services/CloudGrpsService.cs
+++ cloud-server/Services/CloudGrpsService.cs
@@ -5,23 +5,57 @@ namespace cloud_server.Services
 {   
     public class CloudGrpsService: Cloud.CloudBase
     {
+        private AuthDB _db;
         private readonly ILogger<CloudGrpsService> _logger;
 
-        public CloudGrpsService(ILogger<CloudGrpsService> logger)
+        public CloudGrpsService(ILogger<CloudGrpsService> logger, AuthDB db)
         {
             _logger = logger;
+            _db = db;  
         }
       
         public override Task<signupResponse> signup(signupRequest request, ServerCallContext context)
         {
-            // imploment
-            return Task.FromResult(new signupResponse { SessionId = "1" });
+            try
+            {
+                this._db.signup(request.Username, request.Password, request.Email ,(request.PhoneNumber != "")? request.PhoneNumber : "NULL");
+                return Task.FromResult(new signupResponse
+                {
+                    Status = GrpcCloud.Status.Success,
+                    Message = ""
+                });
+
+            }
+            catch (Exception ex)
+            {
+                return Task.FromResult(new signupResponse
+                {
+                    Status = GrpcCloud.Status.Failure,
+                    Message = ex.Message
+                });
+
+            }
         }
 
         public override Task<loginResponse> login(loginRequest request, ServerCallContext context)
         {
-            // imploment
+            try
+            {
+                this._db.login(request.Username, request.Password);
+                return Task.FromResult(new loginResponse
+                {
+                    SessionId = ""
+                });
 
+            }
+            catch (Exception ex)
+            {
+                return Task.FromResult(new loginResponse
+                {
+                    SessionId = ""
+                });
+
+            }
             return Task.FromResult(new loginResponse { SessionId = "1" });
         }
         public override Task<logoutResponse> logout(logoutRequest request, ServerCallContext context)
