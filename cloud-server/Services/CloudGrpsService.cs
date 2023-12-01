69e21eba0fa0d4fa06d2d47ff9159e3aca33ae0d
Aviv Hazan <aviv6715@gmail.com>
2023-12-01 16:39:49 +0200

    Create cloud grpc service class

--- cloud-server/Services/CloudGrpsService.cs
+++ cloud-server/Services/CloudGrpsService.cs
@@ -1,10 +1,9 @@
 ﻿using Grpc.Core;
 using GrpcCloud;
-using System.Threading;
 
 namespace cloud_server.Services
-{
-    public class CloudGrpsService: GrpcCloud.Cloud.CloudBase
+{   
+    public class CloudGrpsService: Cloud.CloudBase
     {
         private readonly ILogger<CloudGrpsService> _logger;
 
@@ -12,7 +11,25 @@ namespace cloud_server.Services
         {
             _logger = logger;
         }
+      
+        public override Task<signupResponse> signup(signupRequest request, ServerCallContext context)
+        {
+            // imploment
+            return Task.FromResult(new signupResponse { SessionId = "1" });
+        }
 
-       
+        public override Task<loginResponse> login(loginRequest request, ServerCallContext context)
+        {
+            // imploment
+
+            return Task.FromResult(new loginResponse { SessionId = "1" });
+        }
+        public override Task<logoutResponse> logout(logoutRequest request, ServerCallContext context)
+        {
+            // imploment
+
+            return Task.FromResult(new logoutResponse());
+
+        }
     }
 }
