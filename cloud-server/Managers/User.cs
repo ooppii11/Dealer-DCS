d2d88de47ee28d1552a30820d17094f475dc6a60
Aviv Hazan <aviv6715@gmail.com>
2023-12-02 12:12:39 +0200

    Create user class

---
+++ cloud-server/Managers/User.cs
@@ -0,0 +1,25 @@
+﻿using System.Xml.Linq;
+
+namespace cloud_server.Managers
+{
+    public class User
+    {
+        private string _id;
+        private string _username;
+        private string _email;
+        private string _phoneNumber;
+
+        public User(string id, string username, string email, string phoneNumber)
+        {
+            this._id = id;
+            this._username = username;
+            this._email = email;
+            this._phoneNumber = phoneNumber;
+        }
+
+        private string Id => _id;
+        private string Username => _username;
+        private string Email => _email;
+        private string Phone => _phoneNumber;
+    }
+}
