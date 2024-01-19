9492ef411cc18dfae62b545d797fd70a1552e2f9
Aviv Hazan <aviv6715@gmail.com>
2024-01-19 17:57:42 +0200

    Update log test

--- node-server/node-server/Managers/Raft/RaftTests/LoggerTest.cs
+++ node-server/node-server/Managers/Raft/RaftTests/LoggerTest.cs
@@ -1,18 +1,19 @@
-﻿namespace NodeServer.Managers.Raft.RaftTests
+﻿/*namespace NodeServer.Managers.Raft.RaftTests
 {
-    /*
+    
     public class LoggerTest
     {
         static void Main(string[] args)
         {
-            Log logger = new Log("x.log");
-            LogEntry logEntry = new LogEntry("1\t2024-01-05T15:30:00.000Z\t1\t127.0.0.1\topertion\topertionData\tfalse");
+            Log logger = new Log("log.log");
+            LogEntry logEntry = new LogEntry("0\t2024-01-05T15:30:00.000Z\t1\t127.0.0.1\topertion\topertionData\tfalse");
 
             logger.AppendEntry(logEntry);
-            logger.CommitEntry(1);
+            logger.CommitEntry(0);
             Console.WriteLine(logger.GetLastLogEntry().ToString());
 
         }
     }
-    */
+    
 }
+*/
