b298bb55afe05ed81d45dbe684d89559ff8b94a9
Itamar <113009227+ooppii11@users.noreply.github.com>
2024-05-28 21:49:04 +0300

    Base xml and code (example)

---
+++ client/Android/StorageAndroidClient/StorageAndroidClient/Login.cs
@@ -0,0 +1,56 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+
+namespace StorageAndroidClient
+{
+    [Activity(Label = "LoginActivity", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class LoginActivity : AppCompatActivity
+    {
+        EditText usernameEditText;
+        EditText passwordEditText;
+        Button loginButton;
+        TextView signupLink;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.login);
+
+            usernameEditText = FindViewById<EditText>(Resource.Id.username);
+            passwordEditText = FindViewById<EditText>(Resource.Id.password);
+            loginButton = FindViewById<Button>(Resource.Id.loginButton);
+            signupLink = FindViewById<TextView>(Resource.Id.signupLink);
+
+            loginButton.Click += LoginButton_Click;
+            signupLink.Click += SignupLink_Click;
+        }
+
+        private void LoginButton_Click(object sender, System.EventArgs e)
+        {
+            string username = usernameEditText.Text;
+            string password = passwordEditText.Text;
+
+            // Perform login logic here
+
+            // If login is successful, save session ID to shared preferences
+            ISharedPreferences prefs = GetSharedPreferences("YourAppPrefs", FileCreationMode.Private);
+            ISharedPreferencesEditor editor = prefs.Edit();
+            editor.PutString("SessionId", "YourSessionId");
+            editor.Apply();
+
+            // Navigate to HomeActivity
+            Intent intent = new Intent(this, typeof(MainActivity));
+            StartActivity(intent);
+            Finish();
+        }
+
+        private void SignupLink_Click(object sender, System.EventArgs e)
+        {
+            Intent intent = new Intent(this, typeof(SignupActivity));
+            StartActivity(intent);
+        }
+    }
+}
---
+++ client/Android/StorageAndroidClient/StorageAndroidClient/Signup.cs
@@ -0,0 +1,55 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using StorageAndroidClient;
+
+namespace StorageAndroidClient
+{
+    [Activity(Label = "SignupActivity")]
+    public class SignupActivity : AppCompatActivity
+    {
+        EditText usernameEditText;
+        EditText emailEditText;
+        EditText passwordEditText;
+        Button signupButton;
+        TextView loginLink;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.signup);
+
+            usernameEditText = FindViewById<EditText>(Resource.Id.username);
+            emailEditText = FindViewById<EditText>(Resource.Id.email);
+            passwordEditText = FindViewById<EditText>(Resource.Id.password);
+            signupButton = FindViewById<Button>(Resource.Id.signupButton);
+            loginLink = FindViewById<TextView>(Resource.Id.loginLink);
+
+            signupButton.Click += SignupButton_Click;
+            loginLink.Click += LoginLink_Click;
+        }
+
+        private void SignupButton_Click(object sender, System.EventArgs e)
+        {
+            string username = usernameEditText.Text;
+            string email = emailEditText.Text;
+            string password = passwordEditText.Text;
+
+            // Perform signup logic here
+
+            // After successful signup, navigate back to LoginActivity
+            Intent intent = new Intent(this, typeof(LoginActivity));
+            StartActivity(intent);
+            Finish();
+        }
+
+        private void LoginLink_Click(object sender, System.EventArgs e)
+        {
+            Intent intent = new Intent(this, typeof(LoginActivity));
+            StartActivity(intent);
+            Finish();
+        }
+    }
+}
